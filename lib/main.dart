ac9f1d8a27a7d94c85e40de9c7a6e7d0637dafe2
Elizabeth Ajileye <ajioluwaseun1@gmail.com>
2024-06-22 12:07:59 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const HomePage(),
+    );
+  }
+}
+
+const names = [
+  'Alice',
+  'Bob',
+  'Charles',
+  'David',
+  'Evalina',
+  'Freddie',
+  'Ginny',
+  'Harry',
+  'Joseph',
+  'Kelly',
+  'Larry'
+      'Mariana'
+];
+
+final tickerProvider = StreamProvider(
+  (ref) => Stream.periodic(
+    const Duration(seconds: 2),
+    (i) => i + 1,
+  ),
+);
+
+// final nameProvider = StreamProvider((ref) {
+//    final a = ref.watch(tickerProvider).map(data: data, error: error, loading: loading)
+// });
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Stream Provider'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
